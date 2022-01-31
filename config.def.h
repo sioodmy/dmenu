798eeff82c6b961d014d93454d1af08e2ea36428
sioodmy <sioodmy@tuta.io>
2022-01-31 21:43:55 +0100

    Added some patches

--- config.def.h
+++ config.def.h
@@ -2,7 +2,8 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -16,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
 /*
  * Characters not considered part of a word while deleting words
