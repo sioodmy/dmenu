e1951bdff44421ddcaea3aa44f2eb0dba70b396a
sioodmy <sioodmy@tuta.io>
2022-02-03 16:29:40 +0100

    fix mouse support

--- dmenu.c
+++ dmenu.c
@@ -147,7 +147,7 @@ drawmenu(void)
 {
 	unsigned int curpos;
 	struct item *item;
-	int x = 0, y = 0, w;
+	int x = 0, y = 0, fh = drw->fonts->h, w;
 
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	drw_rect(drw, 0, 0, mw, mh, 1, 1);
@@ -165,7 +165,7 @@ drawmenu(void)
 	curpos = TEXTW(text) - TEXTW(&text[cursor]);
 	if ((curpos += lrpad / 2 - 1) < w) {
 		drw_setscheme(drw, scheme[SchemeNorm]);
-		drw_rect(drw, 0 + curpos, (prompt ? 2+bh : 2), 2, bh - 4, 1, 0);
+		drw_rect(drw, 0 + curpos, (prompt ? 2 + (bh -fh) / 2 + bh: 2 + (bh - fh) / 2) , 2, fh - 4, 1, 0);
 	}
 
 	if (lines > 0) {
@@ -616,8 +616,8 @@ buttonpress(XEvent *e)
 	if (ev->button == Button3)
 		exit(1);
 
-	if (prompt && *prompt)
-		x += promptw;
+    if (prompt)
+		y += bh;
 
 	/* input field */
 	w = (lines > 0 || !matches) ? mw - x : inputw;
@@ -724,6 +724,9 @@ mousemove(XEvent *e)
 	struct item *item;
 	XPointerMovedEvent *ev = &e->xmotion;
 	int x = 0, y = 0, h = bh, w;
+
+    if (prompt)
+        y+=bh;
 
 	if (lines > 0) {
 		w = mw - x;
@@ -872,6 +875,7 @@ setup(void)
 
 	/* calculate menu geometry */
 	bh = drw->fonts->h + 2;
+	bh = MAX(bh,lineheight);	/* make a menu line AT LEAST 'lineheight' tall */
 	lines = MAX(lines, 0);
 	mh = (lines + 1) * bh;
 	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
@@ -967,7 +971,7 @@ setup(void)
 static void
 usage(void)
 {
-	fputs("usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
+	fputs("usage: dmenu [-bfiv] [-l lines] [-h height] [-p prompt] [-fn font] [-m monitor]\n"
 	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
 	exit(1);
 }
@@ -1034,6 +1038,10 @@ main(int argc, char *argv[])
 		/* these options take one argument */
 		else if (!strcmp(argv[i], "-l"))   /* number of lines in vertical list */
 			lines = atoi(argv[++i]);
+		else if (!strcmp(argv[i], "-h")) { /* minimum height of one menu line */
+			lineheight = atoi(argv[++i]);
+			lineheight = MAX(lineheight, min_lineheight);
+		}
 		else if (!strcmp(argv[i], "-m"))
 			mon = atoi(argv[++i]);
 		else if (!strcmp(argv[i], "-p"))   /* adds prompt to left of input field */
